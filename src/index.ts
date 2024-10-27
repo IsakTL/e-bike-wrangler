adedff64e304a376c1df4bcab2ebfe575f6cbfb1
Isak <isaktlarsson99@gmail.com>
2024-10-26 22:58:49 -0500

    Make those trucks tow

--- src/index.ts
+++ src/index.ts
@@ -8,9 +8,6 @@ import Cli from "./classes/Cli.js";
 // create an array of vehicles
 const vehicles = [];
 
-// TODO: uncomment once trucks are implemented
-// const truck1 = new Truck(Cli.generateVin(),"red", "Ford", "F-150", 2021, 5000, 120, [], 10000);
-
 // will use default wheels
 const car1 = new Car(
   Cli.generateVin(),
@@ -23,15 +20,15 @@ const car1 = new Car(
   []
 );
 
-// TODO: uncomment once motorbikes are implemented
+const truck1 = new Truck(Cli.generateVin(),"red", "Ford", "F-150", 2021, 5000, 120, [], 10000);
+
 const motorbike1Wheels = [new Wheel(17, "Michelin"), new Wheel(17, "Michelin")];
 const motorbike1 = new Motorbike(Cli.generateVin(), "black", "Harley Davidson", "Sportster", 2021, 500, 125, motorbike1Wheels);
 
 // push vehicles to array
-// TODO: uncomment once trucks are implemented
-// vehicles.push(truck1);
+
 vehicles.push(car1);
-// TODO: uncomment once motorbikes are implemented
+vehicles.push(truck1);
 vehicles.push(motorbike1);
 
 // create a new instance of the Cli class
